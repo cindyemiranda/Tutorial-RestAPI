806385951bc9ff8a1eba46965cea43132487e1cb
CindyGB <49107051+cindyemiranda@users.noreply.github.com>
2023-06-13 22:57:09 -0500

    ADD valores a constantes
    Se agregaron valores a las constantes para cambiar el mensaje de bienvenida a la aplicación en cinco idiomas (Castellano, inglés, francés, italiano y portugués).

--- src/main/java/com/tutorial_rest/Tutorial/Rest/utils/Constantes.java
+++ src/main/java/com/tutorial_rest/Tutorial/Rest/utils/Constantes.java
@@ -2,11 +2,11 @@ package com.tutorial_rest.Tutorial.Rest.utils;
 
 public class Constantes {
 	
-	public static final String CODIGO_GENERAL_1	= "";
-	public static final String CODIGO_GENERAL_2	= "";
-	public static final String CODIGO_GENERAL_3	= "";
-	public static final String CODIGO_GENERAL_4	= "";
-	public static final String CODIGO_GENERAL_5	= "";
+	public static final String CODIGO_GENERAL_1	= "Bienvenido";
+	public static final String CODIGO_GENERAL_2	= "Welcome";
+	public static final String CODIGO_GENERAL_3	= "Accueillir";
+	public static final String CODIGO_GENERAL_4	= "Benvenuto";
+	public static final String CODIGO_GENERAL_5	= "Bem-vindo";
 		
 	Constantes() {		
 	}
