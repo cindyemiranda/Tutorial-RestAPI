cb0208f63069f6270b7859736aa6c762840032f3
CindyGB <49107051+cindyemiranda@users.noreply.github.com>
2023-06-13 22:52:41 -0500

    ADD valores a constantes
    Se agregaron valores a las constantes para dar el mensaje de bienvenida en cinco idiomas (castellano, inglés, francés, italiano y portugués)

--- src/main/java/com/tutorial_rest/Tutorial/Rest/controllers/TutorialController.java
+++ src/main/java/com/tutorial_rest/Tutorial/Rest/controllers/TutorialController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tutorial_rest.Tutorial.Rest.messages.ResponseMessage;
+import com.tutorial_rest.Tutorial.Rest.utils.Constantes;
 
 
 
@@ -13,7 +14,7 @@ import com.tutorial_rest.Tutorial.Rest.messages.ResponseMessage;
 public class TutorialController {
 	@GetMapping("/welcome")
 	public ResponseEntity<ResponseMessage> getWelcome(){
-		String mensaje="Welcome!!!";
+		String mensaje=Constantes.CODIGO_GENERAL_1;
 		System.out.println(mensaje);
 		return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(mensaje));
 		
